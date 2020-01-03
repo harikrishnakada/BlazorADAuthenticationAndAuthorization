f830a6090cac7f961fc698d80a2747574cf416ca
Hari Krishna <Harik@touchcoresystems.com>
2020-01-03 21:49:06 +0530

    Added Azure AD Authentication.

--- BlazorADAuthenticationAndAuthorization/Startup.cs
+++ BlazorADAuthenticationAndAuthorization/Startup.cs
@@ -10,6 +10,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BlazorADAuthenticationAndAuthorization.Data;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace BlazorADAuthenticationAndAuthorization
 {
@@ -26,6 +31,30 @@ namespace BlazorADAuthenticationAndAuthorization
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(AzureADDefaults.AuthenticationScheme)
+                .AddAzureAD(options => Configuration.Bind("AzureAd", options));
+
+            services.Configure<OpenIdConnectOptions>(AzureADDefaults.OpenIdScheme, options =>
+            {
+                options.Authority = options.Authority + "/v2.0/";         // Microsoft identity platform
+
+                options.TokenValidationParameters.ValidateIssuer = false; // accept several tenants (here simplified)
+            });
+
+            services.AddControllersWithViews(options =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                options.Filters.Add(new AuthorizeFilter(policy));
+            });
+
+            // Here add your policies for which you want to authorize.
+            //For more information please refer https://docs.microsoft.com/en-us/aspnet/core/security/authorization/policies?view=aspnetcore-3.1
+
+            // ObjectId: You can find it in the groups tab in Azure Ad
+            services.AddAuthorization(o => { o.AddPolicy("Management", pb => pb.RequireClaim("groups", "ENTER OBJECTID OF YOUR SECURITY GROUP")); o.AddPolicy("NonManagement", pb => pb.RequireClaim("groups", "ENTER OBJECTID OF YOUR SECURITY GROUP")); });
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
@@ -50,8 +79,12 @@ namespace BlazorADAuthenticationAndAuthorization
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
